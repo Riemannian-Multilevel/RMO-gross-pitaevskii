0d1425e9cf2d8df21d136144ec61843c9e97ee38
Ferdinand Vanmaele <ferdinand-joseph.vanmaele@uni-a.de>
2026-03-02 09:55:28 +0100

    refactor checkgradient

--- test_gradient.cc
+++ test_gradient.cc
@@ -47,6 +47,7 @@ template <typename PrecondType>
 using InverseOpType = InverseMatrix<OperatorType, PrecondType>;
 
 // Contains all metric-independent components for testing gradients on the ellipsoid
+// TODO: reflect in class name?
 template <int dim>
 class GradientTestBase
 {
@@ -71,8 +72,6 @@ public:
 
         const double factor = x*Mx;
         x /= std::sqrt(factor);
-
-        m_problem.assemble_nonlinear_term(x);  // updates Mpp -> A for underlying operators
     }
 
     void random_tangent_vector(const Vector<double>& x, Vector<double>& v) const
@@ -83,6 +82,11 @@ public:
 
         // 2. project orthogonally onto tangent space at x, wrt. the mass metric
         ellipsoid::project_onto_tangent_space(x, M, tmp, v);
+    }
+
+    void assemble(const Vector<double>& x) const
+    {
+        m_problem.assemble_nonlinear_term(x);  // updates Mpp -> A (mutable) for underlying operators
     }
 
     void to_tangent_space(const Vector<double>& x, const Vector<double>& v, Vector<double>& v_proj) const
@@ -101,10 +105,11 @@ public:
     auto get_M() const { return M; }
     auto get_A_inv() const { return A_inv; }
     auto get_M_inv() const { return M_inv; }
+    auto n_dofs() const { return A.m(); }
 
-    virtual double value(const Vector<double>&) {}
-    virtual Vector<double> gradient(const Vector<double>&) {}
-    virtual double metric(const Vector<double>&, const Vector<double>&) {}
+    virtual double value(const Vector<double>&) const {}
+    virtual Vector<double> gradient(const Vector<double>&) const {}
+    virtual double metric(const Vector<double>&, const Vector<double>&) const {}
 
 protected:
     const GrossPitaevskiiProblem<dim>& m_problem;
@@ -120,21 +125,21 @@ public:
         : GradientTestBase<dim>(problem, beta)
     {}
 
-    double value(const Vector<double>& x) override
+    double value(const Vector<double>& x) const override
     {
         Vector<double> Ax(x.size());
         this->A.vmult(Ax, x);
         return 0.5 * (x * Ax);
     }
 
-    Vector<double> gradient(const Vector<double>& x) override
+    Vector<double> gradient(const Vector<double>& x) const override
     {
         Vector<double> x_grad(x.size());
         ellipsoid::gradient(this->A_inv, this->M, x, x_grad);
         return x_grad;
     }
 
-    double metric(const Vector<double>& y, const Vector<double>& z) override
+    double metric(const Vector<double>& y, const Vector<double>& z) const override
     {
         Vector<double> Az(z.size());
         this->A.vmult(Az, z);
@@ -150,21 +155,21 @@ public:
         : GradientTestBase<dim>(problem, beta)
     {}
 
-    double value(const Vector<double>& x) override
+    double value(const Vector<double>& x) const override
     {
         Vector<double> Ax(x.size());
         this->A.vmult(Ax, x);
         return 0.5 * (x * Ax);
     }
 
-    Vector<double> gradient(const Vector<double>& x) override
+    Vector<double> gradient(const Vector<double>& x) const override
     {
         Vector<double> x_grad(x.size());
         ellipsoid::gradient(this->M_inv, this->A, this->M, x, x_grad);
         return x_grad;
     }
 
-    double metric(const Vector<double>& y, const Vector<double>& z) override
+    double metric(const Vector<double>& y, const Vector<double>& z) const override
     {
         Vector<double> Mz(z.size());
         this->M.vmult(Mz, z);
@@ -180,17 +185,17 @@ public:
         : GradientTestBase<dim>(problem, beta)
     {}
 
-    double value(const Vector<double>& x) override
+    double value(const Vector<double>& x) const override
     {
         return 0;
     }
 
-    Vector<double> gradient(const Vector<double>& x) override
+    Vector<double> gradient(const Vector<double>& x) const override
     {
         return {};
     }
 
-    double metric(const Vector<double>& y, const Vector<double>& z) override
+    double metric(const Vector<double>& y, const Vector<double>& z) const override
     {
         return {};
     }
@@ -204,87 +209,62 @@ public:
         : GradientTestBase<dim>(problem, beta)
     {}
 
-    double value(const Vector<double>& x) override
+    double value(const Vector<double>& x) const override
     {
         return 0;
     }
 
-    Vector<double> gradient(const Vector<double>& x) override
+    Vector<double> gradient(const Vector<double>& x) const override
     {
         return {};
     }
 
-    double metric(const Vector<double>& y, const Vector<double>& z) override
+    double metric(const Vector<double>& y, const Vector<double>& z) const override
     {
         return {};
     }
 };
 
+// Test correctness of gradients for random samples
 template <int dim>
-void check_gradient(const GradientTestBase<dim>& test_grad, std::string filename)
+void check_gradient(const GradientTestBase<dim>& test_grad, unsigned int n_trials, std::string prefix)
 {
-
-}
-
-int main()
-{
-    GPE_Options options{};
-    options.dimension = 2;
-    options.degree    = 1;  // piecewise linear (1) or quadratic (2) elements
-    options.radius    = 10;
-    options.beta      = 100;
-    options.bc        = BoundaryCondition::DIRICHLET;
-    options.mesh_kind = MeshKind::QUADRILATERAL;
-    options.order     = Ordering::CUTHILL_MCKEE;
-
-    constexpr unsigned int n_levels = 8;
-    GrossPitaevskiiPackage<2> GS(options, n_levels);
-    GrossPitaevskiiProblem<2> problem = GS.problem(Square<2>());
-    const unsigned n_dofs = GS.n_dofs();
-
-    // Problem definition
-    // TODO: std::unique_ptr
-    GradientTestMass<2>         test_mass(problem, options.beta);
-    GradientTestEnergy<2>       test_energy(problem, options.beta);
-    GradientTestEnergyCoarse<2> test_energy_coarse(problem, options.beta);
-    GradientTestMassCoarse<2>   test_mass_coarse(problem, options.beta);
-
     dealii::ConvergenceTable convergence_table;
 
-    // Test correctness of gradients for random samples
-    // TODO: input: GradientTestBase
-    for (unsigned int trial = 0; trial < NUM_TRIALS; trial++) {
-        auto filename = fmt::format("checkgradient_trial_{:03}.dat", trial);
+    for (unsigned int trial = 0; trial < n_trials; trial++) {
+        auto filename = prefix + fmt::format("_{:3}.dat",trial);
         std::cerr << "Writing " << filename << std::endl;
         std::ofstream outfile(filename);  // rename according to function/metric used
+        const unsigned int n_dofs = test_grad.n_dofs();
 
         // 1. Generate a random point x in the manifold S
         Vector<double> x(n_dofs);
-        test_energy.random_point(x);  // initializes M_pp
+        test_grad.random_point(x);
+        test_grad.assemble(x);    // initializes M_pp
 
         // Check x fulfills |x|_M = 1
         // TODO: merge to GradientTestBase
         Vector<double> Mx(x.size());
-        test_energy.get_M().vmult(Mx, x);
+        test_grad.get_M().vmult(Mx, x);
         convergence_table.add_value("x_constr", x*Mx);
 
         // 2. Generate a random tangent vector v at x with |v|_x = 1
         Vector<double> v(n_dofs);
-        test_energy.random_tangent_vector(x, v);
-        v /= std::sqrt(test_energy.metric(v, v));  // tangent vector with |v|_x = 1
+        test_grad.random_tangent_vector(x, v);
+        v /= std::sqrt(test_grad.metric(v, v));  // tangent vector with |v|_x = 1
 
         // The Riemannian gradient of E at x is defined as,
         // the unique element in the tangent space at x, T_x S,
         // such that for all v in T_x S,
         // g_x(\grad(x), v) = DE(x)[v]
         // 2b. Verify this condition for finite difference (directional derivative)
-        double fx = test_energy.value(x);
-        Vector<double> x_grad = test_energy.gradient(x);
-        double g_xv = test_energy.metric(x_grad, v);
+        double fx = test_grad.value(x);
+        Vector<double> x_grad = test_grad.gradient(x);
+        double g_xv = test_grad.metric(x_grad, v);
         convergence_table.add_value("grad_xv",g_xv);
 
         // TODO: merge to GradientTestBase
-        auto value = std::bind(&GradientTestEnergy<2>::value, &test_energy, std::placeholders::_1);
+        auto value = std::bind(&GradientTestBase<dim>::value, &test_grad, std::placeholders::_1);
         double dir_xv8 = finite_difference(value, x, v, 1e-8, fx);
         convergence_table.add_value("dir_xv_1e-8",dir_xv8);
 
@@ -293,12 +273,12 @@ int main()
         //    Compute <grad f(x), v>_x
         Vector<double> x_grad_proj(x_grad.size());
         // TODO: Do we care about which metric for the projected gradient?
-        test_energy.to_tangent_space(x, x_grad, x_grad_proj);
+        test_grad.to_tangent_space(x, x_grad, x_grad_proj);
 
         // Residual of difference between gradient, and projected gradient
         Vector<double> x_grad_res(x_grad);
         x_grad_res.add(-1.0, x_grad_proj);
-        convergence_table.add_value("grad_proj_res",std::sqrt(test_energy.metric(x_grad_res,x_grad_res)));
+        convergence_table.add_value("grad_proj_res",std::sqrt(test_grad.metric(x_grad_res,x_grad_res)));
 
         // 4. Compute E(t) for several values of t logarithmically spaced on the interval [10−8,0]
         auto ts = logspace(-8,0,100);
@@ -310,9 +290,9 @@ int main()
             tv *= t;
 
             Vector<double> Rx_tv(x.size());
-            test_energy.retract(x, tv, Rx_tv);
+            test_grad.retract(x, tv, Rx_tv);
 
-            long double Et = std::abs(-test_energy.value(Rx_tv) + fx + t*g_xv);
+            long double Et = std::abs(-test_grad.value(Rx_tv) + fx + t*g_xv);
             //Ets.push_back(Et);
             outfile << t << "\t" << Et << std::endl;
         }
@@ -332,4 +312,29 @@ int main()
     convergence_table.set_scientific("grad_proj_res", true);
 
     convergence_table.write_text(std::cout, dealii::TableHandler::TextOutputFormat::table_with_headers);
+}
+
+int main()
+{
+    GPE_Options options{};
+    options.dimension = 2;
+    options.degree    = 1;  // piecewise linear (1) or quadratic (2) elements
+    options.radius    = 10;
+    options.beta      = 100;
+    options.bc        = BoundaryCondition::DIRICHLET;
+    options.mesh_kind = MeshKind::QUADRILATERAL;
+    options.order     = Ordering::CUTHILL_MCKEE;
+
+    constexpr unsigned int n_levels = 8;
+    GrossPitaevskiiPackage<2> GS(options, n_levels);
+    GrossPitaevskiiProblem<2> problem = GS.problem(Square<2>());
+    const unsigned n_dofs = GS.n_dofs();
+
+    // Problem definition
+    GradientTestEnergy<2> test_energy(problem, options.beta);
+    check_gradient(test_energy, NUM_TRIALS, "checkgradient_trial");
+
+    // GradientTestMass<2>         test_mass(problem, options.beta);
+    // GradientTestEnergyCoarse<2> test_energy_coarse(problem, options.beta);
+    // GradientTestMassCoarse<2>   test_mass_coarse(problem, options.beta);
 }
